48e4a8ec069455eaa3514138813bf24855796bf0
Tsiresy <tsiresyrabekoto@gmail.com>
2020-06-01 05:38:06 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Welcome from './Components/Welcome';
+import Spend from './Components/Spend';
+import Test from './Components/Test';
+import Rest from './Components/Rest';
+import History from './Components/History';
+import Home from './Components/Home';
+
+const App = () => {
+
+  return (
+    <div>
+      <BrowserRouter>
+       
+        <Switch>
+          
+          <Route exact path="/"  component={Welcome} />   
+          <Route  path="/spend"  component={Spend} />   
+          <Route  path="/rest"  component={Rest} />   
+          <Route  path="/history/:page"  component={History} />    
+          {/* <Route exact path="/test"  component={Test} />    */}
+
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
